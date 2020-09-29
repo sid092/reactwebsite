414248d0a8631c945636340c5714d51220730104
krypto <siddharthranjan092@gmail.com>
2020-09-29 15:43:56 +0530

    Updates

---
+++ precache-manifest.23e0de775400722eeea579226f326c95.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777b7d9d679c5e7206a63474dd439dfd",
+    "url": "/reactwebsite/index.html"
+  },
+  {
+    "revision": "8896e835353e7ec6d04a",
+    "url": "/reactwebsite/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "6b577b5d66a2cd9879af",
+    "url": "/reactwebsite/static/css/main.48712627.chunk.css"
+  },
+  {
+    "revision": "8896e835353e7ec6d04a",
+    "url": "/reactwebsite/static/js/2.c7138213.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/reactwebsite/static/js/2.c7138213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b577b5d66a2cd9879af",
+    "url": "/reactwebsite/static/js/main.5adfe665.chunk.js"
+  },
+  {
+    "revision": "b2418759a829b98ef995",
+    "url": "/reactwebsite/static/js/runtime-main.9b48dea8.js"
+  },
+  {
+    "revision": "cda2cbc7aba10a2f6988d183d30ed500",
+    "url": "/reactwebsite/static/media/AI.cda2cbc7.jpg"
+  },
+  {
+    "revision": "7169fc2c72f5c0b44ac30c9819e147b0",
+    "url": "/reactwebsite/static/media/C.7169fc2c.jpg"
+  },
+  {
+    "revision": "157b0d782223c629e000db3f101ef60a",
+    "url": "/reactwebsite/static/media/JavaScript.157b0d78.jpg"
+  },
+  {
+    "revision": "9838f9ebf88ba67d1463febe2f2ad422",
+    "url": "/reactwebsite/static/media/Machine Learning.9838f9eb.jpg"
+  },
+  {
+    "revision": "fed36d150a4217cea203fb2a71e2db7e",
+    "url": "/reactwebsite/static/media/Nodejs.fed36d15.png"
+  },
+  {
+    "revision": "34e2b194a7febd918b00ec2d35a5159e",
+    "url": "/reactwebsite/static/media/android.34e2b194.jpg"
+  },
+  {
+    "revision": "4603f0d1d6482913bccc9693c93bcf5a",
+    "url": "/reactwebsite/static/media/code1.4603f0d1.png"
+  },
+  {
+    "revision": "adc3ece1ac1a4445e9c393d9bee6674d",
+    "url": "/reactwebsite/static/media/office11.adc3ece1.jpg"
+  },
+  {
+    "revision": "dc199ecc9e90e4bc7e9ed9b31e3c2b5f",
+    "url": "/reactwebsite/static/media/python.dc199ecc.png"
+  },
+  {
+    "revision": "bc2f64ff230cbf4b8f6093c574985022",
+    "url": "/reactwebsite/static/media/web-development.bc2f64ff.jpg"
+  }
+]);
